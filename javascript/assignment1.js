13f883d43c5aed44a0b8ca2f82fe1cf34c5f5808
saurabh <saurabhwarhade77@gmail.com>
2022-02-16 14:09:40 +0530

    today's assignment

---
+++ javascript/assignment1.js
@@ -0,0 +1,51 @@
+var x =5;
+var y =1;
+
+var obj ={y:10};
+function a (obj){
+ return   alert(y)
+}
+
+
+console.log(obj);///// it console {y:10} an obj
+a();///// it alert 1
+
+
+
+////////////////////////////////////////////////////
+
+
+var count =0;
+while (count <10) {
+    console.log(count);
+    count++;
+    
+}
+/////it console the loop until condition false 1...9
+
+//////////////////////////////////////////////
+
+ x =9;
+if (x>9){
+    console.log(9)
+}else{
+    console.log(x);
+
+}
+
+//when remove Int then it return 9
+////////////////////////////////////////////////////////////
+
+
+
+var  a1 =[,,,]
+var a2= new Array(3);
+console.log(a1);//empty array
+console.log(a2);///empty array
+console.log(a1[0]);//undefined
+console.log(a1[0]);//undefined  
+
+/////////////////////////////////////////////
+if (!a1[i]) {continue};
+//syntax error :Illegal continue statment.....
+
